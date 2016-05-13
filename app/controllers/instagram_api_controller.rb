3c2477ef802b3413d0f318fd89575aa9bcad5bcf
Rodrigo Troncoso <ratroncoso@uc.cl>
2016-05-13 16:29:16 -0300

    post arreglados

--- app/controllers/instagram_api_controller.rb
+++ app/controllers/instagram_api_controller.rb
@@ -4,16 +4,24 @@ skip_before_filter  :verify_authenticity_token
 require 'httparty'
 
 	def imageGet
-		respond_to do |format|
-			tag = params.require(:tag)
-			token = params.require(:token)
-			#query = {"access_token" => token , "count"=>"20"}
-			#response = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent",:query=>query)
-			response1 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token="+token).parsed_response
-			response2 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"?access_token="+token)
-			#response =  { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
-			format.json {render :json => response1}
-			end
+		tag = params.require(:tag)
+		token = params.require(:token)
+		#query = {"access_token" => token , "count"=>"20"}
+		#response = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent",:query=>query)
+		response1 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token="+token).parsed_response
+		response2 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"?access_token="+token)
+		#response =  { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+		posts= []
+		response1['data'].each do |child|
+			posts<<{
+				:tags =>child['tags'],
+				:username =>child['user']['username'],
+				:likes => child['likes'],
+				:url => child['images']['standard_resolution'],
+				:caption => child['caption']['text']
+			}
+		end
+		render :json => posts
 	end
 end
 	
