b066b2c9a8f33a45b35b345254ad93e5eb509667
Rodrigo Troncoso <ratroncoso@uc.cl>
2016-05-15 16:04:30 -0400

    test postman pasados

--- app/controllers/instagram_api_controller.rb
+++ app/controllers/instagram_api_controller.rb
@@ -5,23 +5,28 @@ require 'httparty'
 
 	def imageGet
 		tag = params.require(:tag)
-		token = params.require(:token)
+		token = params.require(:access_token)
 		#query = {"access_token" => token , "count"=>"20"}
 		#response = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent",:query=>query)
+		version = 1
 		response1 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token="+token).parsed_response
-		response2 = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"?access_token="+token)
+		cantidad = HTTParty.get("https://api.instagram.com/v1/tags/"+tag+"?access_token="+token)
 		#response =  { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
 		posts= []
 		response1['data'].each do |child|
 			posts<<{
 				:tags =>child['tags'],
 				:username =>child['user']['username'],
-				:likes => child['likes'],
-				:url => child['images']['standard_resolution'],
+				:likes => child['likes']['count'	],
+				:url => child['images']['standard_resolution']['url'],
 				:caption => child['caption']['text']
 			}
 		end
-		render :json => posts
+		metadata={:total =>cantidad['data']['media_count']}
+
+		respond_to do |format|
+		format.json {render :json => {metadata: metadata, posts: posts, version: version}}
+		end
 	end
 end
 	
